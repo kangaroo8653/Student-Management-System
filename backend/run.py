eb73539650ddfbbb89757374e9a21821f1dbdca0
kangaroo8653 <kangaroo8653@qq.com>
2024-12-24 16:29:02 +0800

    Update run.py

--- backend/run.py
+++ backend/run.py
@@ -5,8 +5,4 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    # 启动服务器：
-    # host='0.0.0.0' 允许外部访问
-    # port=5000 指定端口号
-    # debug=True 开启调试模式，代码修改后自动重启
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
