cb7a662c5eea0e3be9c4623190c82bb8d1527557
kangaroo8653 <kangaroo8653@qq.com>
2024-12-25 13:52:43 +0800

    Update run.py

--- backend/run.py
+++ backend/run.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     # 从环境变量读取服务器配置
     port = int(os.environ.get('FLASK_PORT', 5000))
     debug = os.environ.get('FLASK_DEBUG', '0') == '1'
-    host = os.environ.get('FLASK_HOST', '0.0.0.0')
+    host = os.environ.get('FLASK_HOST', '127.0.0.1')
     
     # 启动服务器
     app.run(host=host, port=port, debug=debug)
